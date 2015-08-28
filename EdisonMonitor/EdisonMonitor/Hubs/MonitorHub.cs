40416e7847894ff43e7a9913d7fea80b05d781a2
mchuma <Mykhailo.Chuma@eleks.com>
2015-08-28 13:41:58 +0300

    first commit

---
+++ EdisonMonitor/EdisonMonitor/Hubs/MonitorHub.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Collections.Generic;
+using Microsoft.ServiceBus.Messaging;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace EdisonMonitor
+{
+	public class MonitorHub : Hub
+	{
+        public static List<string> Messages { get; set; }
+
+        public void SendMessage(string message)
+        {
+            //RefreshData();
+            // Call the broadcastMessage method to update clients.
+            Clients.All.showMessageOnClient(message  + DateTime.Now.ToString());  //GetMessageText());
+        }
+
+        public void RefreshData()
+        {
+            Messages = new List<string>();
+            string eventHubConnectionString = "Endpoint=sb://inteledisoneventhub-ns.servicebus.windows.net/;SharedAccessKeyName=SendReceiveRule;SharedAccessKey=29SkhvIFngQqIJ8Cv9Lz0Luo+EFVsyZYIycUNaB3ffM=";
+            string eventHubName = "edisoneventhub";
+            string storageAccountName = "edisoneventhubstorage";
+            string storageAccountKey = "wH/HCkHLoPH93xCZwFMR4V4OZE7nbmps1+sFaqutt1mhBjdVdWd+ECuo+c0GbmhB9RsOxFiCB4XEsxoizMksAw==";
+            string storageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}",
+            storageAccountName, storageAccountKey);
+
+            string eventProcessorHostName = Guid.NewGuid().ToString();
+            EventProcessorHost eventProcessorHost = new EventProcessorHost(eventProcessorHostName, eventHubName, EventHubConsumerGroup.DefaultGroupName, eventHubConnectionString, storageConnectionString);
+            var a = eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>();
+            if (!a.IsCompleted)
+            {
+                Clients.All.showMessageOnClient("Waiting..." + DateTime.Now.ToString());
+                Thread.Sleep(1000);
+
+            }
+            Clients.All.showMessageOnClient(GetMessageText() + DateTime.Now.ToString());
+        }
+
+        private string GetMessageText()
+        {
+            string result = "abc";
+            if (SimpleEventProcessor.Messages != null && SimpleEventProcessor.Messages.Count > 0)
+            {
+                foreach (var messageItem in SimpleEventProcessor.Messages)
+                {
+                    result += messageItem + Environment.NewLine;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ EdisonMonitor/EdisonMonitor/Hubs/SimpleEventProcessor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+using System.Diagnostics;
+
+namespace EdisonMonitor
+{
+    public class SimpleEventProcessor : IEventProcessor
+    {
+        Stopwatch checkpointStopWatch;
+        public static List<string> Messages;
+
+        public async Task CloseAsync(PartitionContext context, CloseReason reason)
+        {
+            //Console.WriteLine(string.Format("Processor Shuting Down. Partition ‘{0}’, Reason: ‘{1}’.", context.Lease.PartitionId, reason.ToString()));
+            if (reason == CloseReason.Shutdown)
+            {
+                await context.CheckpointAsync();
+            }
+        }
+
+        public Task OpenAsync(PartitionContext context)
+        {
+            //Console.WriteLine(string.Format("SimpleEventProcessor initialize. Partition: ‘{0}’, Offset: ‘{1}'", context.Lease.PartitionId, context.Lease.Offset));
+            this.checkpointStopWatch = new Stopwatch();
+            this.checkpointStopWatch.Start();
+            return Task.FromResult<object>(null);
+        }
+
+        public async Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+        {
+            if (Messages == null || Messages.Count() == 0)
+            {
+                Messages = new List<string>();
+            }
+
+            foreach (EventData eventData in messages)
+            {
+                string data = Encoding.UTF8.GetString(eventData.GetBytes());
+                Messages.Add(data);
+                //Console.WriteLine(string.Format("Message received. Partition: ‘{0}’, Data: ‘{1}'", context.Lease.PartitionId, data));
+            }
+
+            //Call checkpoint every 5 minutes, so that worker can resume processing from the 5 minutes back if it restarts.
+            if (this.checkpointStopWatch.Elapsed > TimeSpan.FromMinutes(2))
+            {
+                await context.CheckpointAsync();
+                lock (this)
+                {
+                    this.checkpointStopWatch.Reset();
+                }
+            }
+        }
+
+    }
+}
